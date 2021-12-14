52ccc1b39459b87f6f98ff5fa92e953791cadb7e
ayaf-bless <blessambel1@gmail.com>
2021-12-15 01:03:16 +0300

    set up an apollo-server

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello",
+  },
+};
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(() => console.log("Server has started on 4000"));

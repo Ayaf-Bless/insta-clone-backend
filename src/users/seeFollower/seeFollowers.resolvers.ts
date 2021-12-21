dcf3851b8bf4961630e19e8a8f20de3bbdd0c1fa
ayaf-bless <blessambel1@gmail.com>
2021-12-21 03:01:05 +0300

    implemented offset pagination for seeFollowers

--- src/users/seeFollower/seeFollowers.resolvers.ts
+++ src/users/seeFollower/seeFollowers.resolvers.ts
@@ -3,13 +3,32 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { id, page }, context) => {
-      const followers = await client.user
-        .findUnique({ where: { id } })
-        .followers({ skip: (page - 1) * 5, take: 5 });
-      return {
-        ok: true,
-        followers,
-      };
+      try {
+        const user = await client.user.findUnique({
+          where: { id },
+          select: { id: true },
+        });
+        if (!user) {
+          return {
+            ok: false,
+            error: "no user found",
+          };
+        }
+        const followers = await client.user
+          .findUnique({ where: { id } })
+          .followers({ skip: (page - 1) * 5, take: 5 });
+        const totalPages = await client.user.count({ where: { id } });
+        return {
+          ok: true,
+          followers,
+          totalPages,
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: "something went wrong",
+        };
+      }
     },
   },
 };

70cbe4b10203b786190833ddc2f3683c0cf34a15
ayaf-bless <blessambel1@gmail.com>
2022-01-03 00:22:57 +0300

    implement listning to the room that exist

--- src/messages/roomUpdates/roomupdates.resolvers.ts
+++ src/messages/roomUpdates/roomupdates.resolvers.ts
@@ -1,16 +1,26 @@
 import { withFilter } from "graphql-subscriptions";
 import { NEW_MESSAGE } from "../../Interfaces";
 import pubSub from "../../pubSub";
+import client from "../../client";
 
 const resolver = {
   Subscription: {
     roomUpdates: {
-      subscribe: withFilter(
-        () => pubSub.asyncIterator(NEW_MESSAGE),
-        ({ roomUpdates }, { roomId }) => {
-          return roomUpdates.roomId === roomId;
+      subscribe: async (parent, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: { id: args.roomId },
+          select: { id: true },
+        });
+        if (!room) {
+          throw new Error("access refused");
         }
-      ),
+        return withFilter(
+          () => pubSub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates }, { roomId }) => {
+            return roomUpdates.roomId === roomId;
+          }
+        )(parent, args, context, info);
+      },
     },
   },
 };
